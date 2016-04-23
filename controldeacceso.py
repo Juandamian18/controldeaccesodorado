3d69c360a47e845496e91dacb5392e5ed9e8616d
Damián Lucero <juandamianl@hotmail.com>
2016-04-23 12:45:19 -0300

    first commit

--- controldeacceso.py
+++ controldeacceso.py
@@ -2,9 +2,16 @@ import display
 import RPi.GPIO as GPIO
 
 def main():
-
     try:
         while True:
             display.init
             display.lcdWriteFirstLine("Prueba")
             time.sleep(3)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        pass
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    debug("----------========== Starting session! ==========----------")
+    main()
