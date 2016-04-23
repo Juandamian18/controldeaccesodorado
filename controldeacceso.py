9910655df27baa9a1bd648af75389bb3d1801cf1
Damián Lucero <juandamianl@hotmail.com>
2016-04-23 12:37:44 -0300

    Commit message

--- controldeacceso.py
+++ controldeacceso.py
@@ -1,13 +1,10 @@
-import displayController
+import display
 import RPi.GPIO as GPIO
 
 def main():
 
     try:
-        displayController.lcd_init
         while True:
-            displayController.lcd_string("Prueba",LCD_LINE_1)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        pass
-    GPIO.cleanup()
+            display.init
+            display.lcdWriteFirstLine("Prueba")
+            time.sleep(3)
