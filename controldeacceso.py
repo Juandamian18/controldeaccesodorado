9604632a96b33af2efdf4dd4f6f68b5192284801
Damián Lucero <juandamianl@hotmail.com>
2016-04-23 13:58:52 -0300

    test displayController

--- controldeacceso.py
+++ controldeacceso.py
@@ -1,24 +1,27 @@
 import displayController
 import RPi.GPIO as GPIO
+import time
+
+LCD_CMD = False
+
+LCD_LINE_1 = 0x80  # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0  # LCD RAM address for the 2nd line
+
 
 def main():
-    displayController.lcd_init
-    try:
-        while True:
-            display.lcd_string("Rasbperry Pi",LCD_LINE_1)
-            time.sleep(3)
-    except KeyboardInterrupt:
-        GPIO.cleanup()
-        pass
-    GPIO.cleanup()
+    displayController.lcd_init()
+    while True:
+        displayController.lcd_string("Rasbperry Pi", LCD_LINE_1)
+        time.sleep(3)
+
 
 if __name__ == '__main__':
 
-  try:
-    main()
-  except KeyboardInterrupt:
-    pass
-  finally:
-    lcd_byte(0x01, LCD_CMD)
-    lcd_string("Goodbye!",LCD_LINE_1)
-    GPIO.cleanup()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        displayController.lcd_byte(0x01, LCD_CMD)
+        displayController.lcd_string("Goodbye!", LCD_LINE_1)
+        GPIO.cleanup()
